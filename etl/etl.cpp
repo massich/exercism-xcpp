3732e56cf0c7e1d9a21a22728e6547557f989631
Joan Massich <sik@visor.udg.edu>
2016-08-31 15:07:15 +0200

    [etl] initial commit
    I had lots of troubles, to build this hello world. The main problem was
    that I was using BOOST-1.61.0, and the testing code fails.
    I was not able to understand the error message of the compiler since it
    was this sort of template errors.
    
    I've installed BOOST-1.55.0, and it worked like a champ. The problem was
    not mine.

---
+++ etl/etl.cpp
@@ -0,0 +1,8 @@
+#include "etl.h"
+
+std::map<char, int> etl::transform(std::map<int, std::vector<char> > xx)
+{
+  std::cout <<"hi from transform" << std::endl;
+  std::map<char, int> puta{{'A', 1}};
+  return puta;
+}
