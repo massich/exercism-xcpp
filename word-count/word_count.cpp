acd6eeddcc4f3922a3dc046855d7ff4ccfd51c27
Joan Massich <sik@visor.udg.edu>
2016-09-21 14:31:33 +0200

    [words-count] Clean debug messages

--- word-count/word_count.cpp
+++ word-count/word_count.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <regex>
 #include "word_count.h"
-#include <iostream>
 
 
 std::map<std::string, int> word_count::words(std::basic_string<char> const &s)
@@ -13,29 +12,17 @@ std::map<std::string, int> word_count::words(std::basic_string<char> const &s)
   auto words_end = std::sregex_iterator();
   std::map<std::string, int> word_occurrences;
 
-  std::cout << "sentence :" << s <<std::endl;
-  std::size_t counter=0;
   for (auto words_it = words_begin; words_it != words_end; ++words_it)
     {
-      std::cout << "iteration " << counter;
       std::string crr_word = words_it->str();
-      std::cout << "reads \"" << crr_word << "\"" ;
       std::transform(crr_word.begin(), crr_word.end(), crr_word.begin(), ::tolower);
-      std::cout << "transform \"" << crr_word << "\"" ;
+
       auto it = word_occurrences.find(crr_word);
       if (it != word_occurrences.end())
          it->second++;
       else
         word_occurrences.emplace(crr_word, 1);
-      std::cout << "occurrences {";
-      for (auto const &occ : word_occurrences)
-        std::cout << "( " << occ.first << ", " << occ.second << " )" ;
-
-      std::cout <<"}"<< std::endl;
     }
-  std::cout << "occ :" ;
-  for (auto const &occ : word_occurrences)
-    std::cout << "( " << occ.first << ", " << occ.second << " )" ;
-  std::cout <<"}"<< std::endl;
+
   return word_occurrences;
 }
