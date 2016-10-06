30ef2182fedc860de7ef2787bfee7c1986e36237
Joan Massich <sik@visor.udg.edu>
2016-10-06 17:33:27 +0200

    [hamming] Fixed throw exception.
    `new` is not needed when throwing exceptions.

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -4,7 +4,8 @@
 
 ::size_t hamming::compute(const std::string& s, const std::string& z)
 {
-  if ( s.length() != z.length() ) throw new std::domain_error("unequal length");
+  if ( s.length() != z.length() )
+    throw std::domain_error("unequal length");
   auto z_it = z.begin();
   auto my_comparison = [&](::size_t sum, char x) { return sum + (x != *z_it++); };
   return accumulate( s.begin(), s.end(), 0, my_comparison);
--- hamming/hamming.h
+++ hamming/hamming.h
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #include <string>
-#include <iostream>
 
 namespace hamming {
   ::size_t compute(const std::string& s, const std::string& z);
